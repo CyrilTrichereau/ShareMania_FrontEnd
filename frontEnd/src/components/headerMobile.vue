3f5f5e9abb7bff4ca0e3b799d1be4cd5612fec04
Cyril Trichereau <cyril.trichereau@yahoo.fr>
2021-07-15 21:58:58 +0200

    custom bootstrap

---
+++ frontEnd/src/components/headerMobile.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="bg-primary text-light fixed-top">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+
+    </div>
+<h1 class="text-center">C'est ici que ça se passe !</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMobile'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- frontEnd/src/views/Home.vue
+++ frontEnd/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
+    <HeaderMobile/>
     <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +11,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMobile from '@/components/headerMobile.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld, HeaderMobile
   }
 }
 </script>
